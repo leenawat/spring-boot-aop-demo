390db08c2840b3c8906c8cbbab40561dbd36f11e
leenawat <leenawat.pa@gmail.com>
2020-07-14 13:21:06 +0700

    init project

---
+++ src/main/java/com/example/springbootaopdemo/SpringBootAopDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootaopdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAopDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAopDemoApplication.class, args);
+	}
+
+}
